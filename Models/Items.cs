9207b6b70c381215d2e115ae2b1ff4013de1755e
Erin Deuel <end.gaia@gmail.com>
2019-10-11 10:11:19 -0700

    add stuff to Items.cs

--- Models/Items.cs
+++ Models/Items.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 namespace VendorOrder.Models
 {
     public class Order{
+        public string VendorName {get; set;}
+        public string VenderDescription {get; set;}
+        public string OrderedGoods {get; set;}
+        public int Id {get;}
 
+        private static List<Order> _Orders = new List<Order>();
+
+        public Order(string vendorName, string orderedGoods)
+        {
+            VendorName = vendorName;
+            OrderedGoods = orderedGoods;
+            _Orders.Add(this);
+        }
     }
 }
